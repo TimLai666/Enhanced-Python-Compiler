b0602f6b1feb1aec9e50289a2df6e2bc4bdaedfc
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-29 00:02:48 +0800

    upload

--- internal/runtime/cpython_windows.go
+++ internal/runtime/cpython_windows.go
@@ -1,8 +1,11 @@
+//go:build windows
+// +build windows
+
 package runtime
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/../../python_includes
-#cgo LDFLAGS: -L${SRCDIR}/../../python_libs -lpython3.12
+#cgo LDFLAGS: -L${SRCDIR}/../../python_libs/windows -lpython312 -static
 #include <Python.h>
 
 void executePythonCode(const char* code) {
@@ -15,5 +18,5 @@ import "C"
 
 func ExecuteCPython(code string) (string, error) {
 	C.executePythonCode(C.CString(code))
-	return "Executed by CPython", nil
+	return "Executed by CPython on Windows", nil
 }
