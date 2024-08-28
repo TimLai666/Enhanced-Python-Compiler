2a8fbb3f2801df38fc6d1c8665db8c57ccba8dc6
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-08-29 00:16:36 +0800

    Update executor.go

--- internal/executor/executor.go
+++ internal/executor/executor.go
@@ -3,12 +3,12 @@ package executor
 import (
 	"enhanced_python_compiler/internal/runtime"
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 func Execute(filePath string) (string, error) {
 	// 讀取 Python 腳本
-	sourceCode, err := ioutil.ReadFile(filePath)
+	sourceCode, err := os.ReadFile(filePath)
 	if err != nil {
 		return "", fmt.Errorf("Error reading file: %v", err)
 	}
